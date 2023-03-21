12cef1b4aa3ddb5c20a429efc80037f4d7e51115
@bhi <asalunke263@gmail.com>
2023-03-22 00:19:57 +0530

    first commit

---
+++ src/main/java/kjdsvk/Serversoftware.java
@@ -0,0 +1,25 @@
+package kjdsvk;
+
+class Theatre
+{
+	void cinema() {
+		System.out.println("Evng show");
+	}
+}
+
+
+
+
+public class Serversoftware {
+	public static void main(String[] args) {
+		Theatre t=new Theatre();
+		customer.bookticket(t);
+		} 
+}
+
+class customer {
+	static void bookticket(Theatre a)
+	{
+		a.cinema();
+	}
+}
